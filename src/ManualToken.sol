65f830ebef65a8a6150f58aaeb9314d9ecfe56bf
无天 <wangqylife@gmail.com>
2024-06-12 16:30:34 +0800

    2

---
+++ src/ManualToken.sol
@@ -0,0 +1,29 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract ManualToken{
+    function name() public  returns (string memory){
+        return "Manual Token";
+    }
+}
