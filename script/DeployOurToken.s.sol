66a9bcbef6ead2890a8490d6a54969fd61a4d108
无天 <wangqylife@gmail.com>
2024-06-12 17:02:42 +0800

    1

--- script/DeployOurToken.s.sol
+++ script/DeployOurToken.s.sol
@@ -20,10 +20,16 @@
 // view & pure functions
 
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.18;
-import {ERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-contract OurToken is ERC20 {
-    constructor(uint256 initCount) ERC20('OurToken', 'OTK') {
-        _mint(msg.sender, initCount);
+import {Script} from 'forge-std/Script.sol';
+import {OurToken} from '../src/OurToken.sol';
+contract DeployOurToken is Script {
+    uint256 public constant INITCOUNT = 1000;
+    function run() public {
+        vm.startBroadcast();
+        new OurToken(INITCOUNT);
+        vm.stopBroadcast();
+        // token.transfer(msg.sender, 1000);
     }
 }
